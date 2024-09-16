beb0ea9ae90ca8d94956712f2819fd896786b75e
NikolasSucadorni <nikolassucadorni@gmail.com>
2024-09-16 09:20:59 -0300

    update

--- dados.py
+++ dados.py
@@ -1,5 +1,23 @@
 import pandas as pd
-import matplotlib as plt
+import matplotlib.pyplot as plt
+import mysql.connector
 
-df = pd.read_csv('')
-df
+mydb = mysql.connector.connect(
+    host = "banco do cliente",
+    user = "usuário integrador",
+    password = "senha",
+    database = "banco de dados"
+)
+
+mycursor = mydb.cursor()
+mycursor.execute("SELECT * FROM tabela")
+result = mycursor.fetchall()
+
+df = pd.DataFrame(result, columns=["coluna x", "coluna y", "coluna z"])
+
+#supondo que o cliente tenha um faturamento e tenhamos valores e datas podemos plotar uma análise simples
+plt.plot(df["data"], df["valor"])
+plt.xlabel("data")
+plt.ylabel("valor")
+plt.title("Faturamento em anos Cliente")
+plt.show
